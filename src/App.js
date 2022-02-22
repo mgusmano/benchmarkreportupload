2ed923d855c7480d2fcee9f2f6f42075721d9b36
Marc Gusmano <mfusmano@yahoo.com>
2022-02-22 10:18:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,173 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Dropzone from 'react-dropzone';
+import readXlsxFile from 'read-excel-file';
+import { checkExcelFile, getCurrentQuarter } from './commonHelper.jsx';
+//import { classes } from './AppCSS';
+import UploadImg from './FileUpload.svg';
+import Select from 'react-select';
+import {
+  getAllPosition,
+  getAllSkill,
+  getAllUser,
+  getPositionTarget,
+  getUserSkill
+} from './AppHelper'
 
-function App() {
+export const App = (props) => {
+  const [selectedfilename, setSelectedFileName] = useState(null);
+    const [selectedquarter, setSelectedQuarter] = useState(null);
+    const [selectedyear, setSelectedYear] = useState(null);
+
+    const [allposition, setAllPosition] = useState(null);
+    const [allskill, setAllSkill] = useState(null);
+    const [alluser, setAllUser] = useState(null);
+    const [positiontarget, setPositionTarget] = useState(null);
+    const [userskill, setUserSkill] = useState(null);
+
+    useEffect(() => {
+      const currentYear = new Date().getFullYear();
+      const quarter = getCurrentQuarter();
+      setSelectedYear({ label: currentYear, value: currentYear })
+      setSelectedQuarter({label: quarter, value: quarter})
+    },[])
+
+    const changeSelectedQuarter = (option) => {
+      console.log('changeSelectedQuarter')
+      setSelectedQuarter(option)
+  }
+
+    const handleDrop = (accepted, rejected) => {
+        let error = {};
+        let selectedFile = null;
+        let selectedFileName = '';
+
+        if (rejected.length > 0) {
+            error = {
+                status: true,
+                message: 'Uploaded file is over 10 MB and is therefore rejected.'
+            };
+        } 
+        else 
+        if (accepted.length > 0) {
+            const isExcel = checkExcelFile(accepted[0].type, accepted[0].name);
+            if (isExcel) {
+                [selectedFile] = accepted;
+                console.log(accepted)
+                selectedFileName = accepted[0].name;
+            } else {
+                error = {
+                    status: true,
+                    message: 'Unsupported File Format.'
+                };
+            }
+        }
+        setSelectedFileName(selectedFileName)
+
+        var file = accepted[0]
+        doFile(file)
+        //this.setState({ selectedFile, error: error, selectedFileName });
+    }
+
+    // const onInputChange = async (e) => {
+    //     var file = e.target.files[0]
+    //     doFile(file)
+    // }
+
+    const doFile = async (file) => {
+        var targetsRows = await readXlsxFile(file, { sheet: "Targets" })
+        var ratingsRows = await readXlsxFile(file, { sheet: "Ratings" })
+
+        var allPosition = getAllPosition(targetsRows); //1
+        var allSkill = getAllSkill(ratingsRows); //2
+        var allUser = getAllUser(ratingsRows, allPosition, selectedquarter, selectedyear); //3
+        var positionTarget = getPositionTarget(targetsRows); //4
+        var userSkill = getUserSkill(ratingsRows, false, selectedquarter, selectedyear); //5
+  
+        setAllPosition(window.URL.createObjectURL(new Blob([JSON.stringify(allPosition,null,2)])))
+        setAllSkill(window.URL.createObjectURL(new Blob([JSON.stringify(allSkill,null,2)])))
+        setAllUser(window.URL.createObjectURL(new Blob([JSON.stringify(allUser,null,2)])))
+        setPositionTarget(window.URL.createObjectURL(new Blob([JSON.stringify(positionTarget,null,2)])))
+        setUserSkill(window.URL.createObjectURL(new Blob([JSON.stringify(userSkill,null,2)])))
+
+        var selfratingsRows = await readXlsxFile(file, { sheet: "SelfRatings" })
+        console.log('selfratingsRows')
+
+    }
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={{display:'flex',flexDirection:'column',border:'10px solid green',width:'100%',height:'100%',boxSizing:'border-box'}}>
+          
+      <div style={{height:'70px',display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <div style={{fontSize:'24px'}}>Upload File for Benchmark Report</div>
+      </div>
 
-export default App;
+      <div style={{height:'70px',display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
+        <div style={{width:'200px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+          <div>Year</div>
+          <Select
+            name="form-field-name"
+            value={selectedyear}
+            className="search-select"
+            optionalClassName="form-select-option"
+            disabled={true}
+            clearable={false}
+          />
+        </div>
+        <div style={{width:'200px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+          <div>Quarter</div>
+          <Select
+            name="form-field-name"
+            value={selectedquarter}
+            onChange={changeSelectedQuarter}
+            className="search-select"
+            optionalClassName="form-select-option"
+            options={[
+              {label: 1, value: 1},
+              {label: 2, value: 2},
+              {label: 3, value: 3},
+              {label: 4, value: 4}
+            ]}
+            clearable={false}
+          />
+        </div>
+      </div>
+
+      <div style={{border:'0px solid red',display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <Dropzone onDrop={handleDrop} multiple={false} maxSize={10000000}>
+          {({getRootProps, getInputProps}) => (  
+            <div style={{margin:'10px 10px 10px 10px',border:'1px dashed blue',cursor:'pointer',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <div style={{margin:'10px 10px 10px 10px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+                <div>Drop an Excel Spreadsheet file here</div>
+                <div>or</div>
+                <div>Choose an Excel Spreadsheet file to upload</div>
+              </div>
+              <img style={{height:'100px'}} src={UploadImg} alt="Data upload"/>
+              <div style={{margin:'10px 10px 10px 10px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+                <div>Supported Format: xlsx</div>
+                <div style={{height:'20px'}}>
+                  {selectedfilename !== null &&
+                  <div>Selected File: {selectedfilename}</div>
+                  }
+                </div>
+              </div>
+            </div>           
+          )}               
+        </Dropzone>
+      </div>
+
+      <div style={{margin:'10px 10px 10px 10px',border:'0px solid red',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+        Downloadable Files:
+      </div>
+
+      <div style={{margin:'10px 10px 10px 10px',border:'0px solid red',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}}>
+        <div>{allposition !== null && <a download="getAllPosition.json" href={allposition}>getAllPosition.json</a>}</div>
+        <div>{allskill !== null && <a download="getAllSkill.json" href={allskill}>getAllSkill.json</a>}</div>
+        <div>{alluser !== null && <a download="getAllUser.json" href={alluser}>getAllUser.json</a>}</div>
+        <div>{positiontarget !== null && <a download="getPositionTarget.json" href={positiontarget}>getPositionTarget.json</a>}</div>
+        <div>{userskill !== null && <a download="getUserSkill.json" href={userskill}>getUserSkill.json</a>}</div>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/AppHelper.js
@@ -0,0 +1,156 @@
+const convertRowNumberToNumber = (val) => {
+    return val-1
+}
+
+const convertColumnLetterToNumber = (str) => {
+    str = str.toUpperCase();
+    let out = 0, len = str.length;
+    for (var pos = 0; pos < len; pos++) {
+      out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - pos - 1);
+    }
+    return out-1;
+}
+
+export const getAllPosition = (rows) => {
+    var dataStartRow = convertRowNumberToNumber(2)
+    var theData = []
+    for (let r = dataStartRow; r < rows.length; r++) {
+        theData.push({
+            "position_id": rows[r][0],
+            "position_name": rows[r][1]
+        })
+    }
+    return theData
+}
+
+export const getAllSkill = (rows) => {
+    var dataStartRow = convertRowNumberToNumber(10)
+    var skillRow = convertRowNumberToNumber(8)
+    var dataStartColumn = convertColumnLetterToNumber('L')
+
+    //var skillJsonData = {};
+    const skillData = [...rows.splice(0, dataStartRow)];
+    const totalIterations = skillData[0].length;
+    let segment = "";
+    let isCore = false;
+    let line = "";
+    let competency = "";
+    //let result = [];
+    let theData = [];
+  
+    for (let index = dataStartColumn; index < totalIterations; index++) {
+      if (skillData[0][index] && segment !== skillData[0][index]) {
+        segment = skillData[0][index];
+      }
+  
+      if (skillData[1][index]) {
+        if (skillData[1][index] === "Core") {
+          isCore = true;
+        } else {
+          isCore = false;
+        }
+      }
+  
+      if (skillData[2][index] && line !== skillData[1][index]) {
+        line = skillData[2][index];
+      }
+  
+      if (skillData[3][index] && competency !== skillData[3][index]) {
+        competency = skillData[3][index];
+      }
+
+      const data2 = {
+        skill_id: skillData[skillRow][index],
+        skill_name: skillData[skillRow+1][index],
+        segment: segment,
+        is_core: isCore ? 1 : 0,
+        line: line,
+        competency: competency,
+      };
+      theData.push(data2);
+      //skillJsonData[data.skillName] = data.skillId;
+    }
+   return theData
+}
+
+export const getAllUser = (rows, positions, selectedquarter, selectedyear) => {
+    console.log('getAllUser')
+    console.log('positions',positions)
+    var dataStartRow = convertRowNumberToNumber(1)
+    var theData = []
+    for (let r = dataStartRow; r < rows.length; r++) {        
+        const found = positions.find(element => element.position_name === rows[r][4]);
+        //console.log(found)
+        var positionId = ''
+        if (found === undefined) {
+            positionId = null
+            console.log('undefined',rows[r][4])
+        }
+        else {
+            positionId = found.position_id
+        }
+        theData.push({
+            "user_index": null,
+            "user_id": rows[r][0],
+            "user_name": rows[r][1],
+            "manager_name": rows[r][2],
+            "position_id": positionId,
+            "position_name": rows[r][4],
+            "is_eb": rows[r][3] === 'EB' ? 1 : 0,
+            "geo_location": rows[r][5],
+            "average": rows[r][6],
+            "user_quarter": selectedquarter.value,
+            "user_year": selectedyear.value,
+            "job_band": 0, //null,
+            "segment": 0, //null,
+            "sub_function":0, // null,
+            "function": 0 //null
+        })
+    }
+    return theData
+}
+
+export function getPositionTarget(rows) {
+    var dataStartRow = convertRowNumberToNumber(2)
+    const totalIterations = rows[dataStartRow].length;
+    var dataStartColumn = convertColumnLetterToNumber('C')
+    let theData = [];
+    for (let r = dataStartRow; r < rows.length; r++) {
+        var skillId = 1
+        for (let c = dataStartColumn; c < totalIterations; c++) {
+            var o = {
+                "position_id": rows[r][0],
+                "skill_id": skillId,
+                "target_rating": rows[r][c]
+            }
+            theData.push(o)
+            skillId++;
+        }
+    }
+    return theData
+}
+
+export const getUserSkill = (rows, isSelfRating, selectedquarter, selectedyear) => {
+    var dataStartRow = convertRowNumberToNumber(10)
+    var skillRow = convertRowNumberToNumber(8)
+    var dataStartColumn = convertColumnLetterToNumber('L')
+    var theData = []
+    for (let r = dataStartRow; r < rows.length; r++) {
+        let userId = rows[r][0]; //row,col
+        // console.log(userId)
+        if (userId) {
+            for (let c = dataStartColumn; c < rows[0].length; c++) {
+                theData.push({
+                    "user_id": userId,
+                    "skill_id": rows[skillRow][c],
+                    "rating": rows[r][c] >= 0 ? rows[r][c] : null,
+                    "is_self": isSelfRating ? 1 : 0,
+                    "user_year": selectedyear.value,
+                    "user_quarter": selectedquarter.value
+                })
+            }
+        }
+    }
+    return theData
+}
+
